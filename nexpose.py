63835bdeda6d494d49d638fc24d413ad11af55d6
Nick Levesque <nick.levesque@gmail.com>
2015-08-06 19:30:20 -0400

    make login and logout use api_request

--- nexpose.py
+++ nexpose.py
@@ -26,7 +26,7 @@ class Nexpose:
 		post_data = (xml_string).encode('utf-8')
 
 		# Prepare the request.
-		request = urllib.request.Request(url)
+		request = urllib.request.Request(self.url)
 		request.add_header("Content-type", "text/xml")
 
 		# Get a response.
@@ -35,61 +35,26 @@ class Nexpose:
 
 		# Check for errors and return response.
 		if not xml_response.tag == 'Failure':
-			return response
+			return xml_response
 		else:
 			for exception in xml_response.iter('Exception'):
 				for message in exception.iter('Message'):
-					return str("Failure: " + message.text)
+					raise  Exception(str("Failure: " + message.text))
 
 	# Login function, we must capture the session-id contained in the response if successful.
 	def login(self, username, password):
-		# Encode the login request string so that urllib will accept it.
 		xml_string = "<LoginRequest user-id=\"%s\" password=\"%s\" />" % (username, password)
-		post_data = (xml_string).encode('utf-8')
-
-		# Prepare the request
-		request = urllib.request.Request(self.url)
-		request.add_header("Content-type", "text/xml")
-
-		# Get a response 
-		response = urllib.request.urlopen(request, post_data, context=self.ctx).read()
-		xml_response = etree.fromstring(response)
-
-		# Check for errors and set session-id.
-		if not xml_response.tag == 'Failure':
-			self.session_id = xml_response.attrib.get('session-id')
-		else:
-			for exception in xml_response.iter('Exception'):
-				for message in exception.iter('Message'):
-					return str("Failure: " + message.text)
+		xml_response = self.api_request(xml_string)
+		self.session_id = xml_response.attrib.get('session-id')
 
 	def logout(self):
-		# Encode the logout request string so that urllib will accept it.
 		xml_string = "<LogoutRequest session-id=\"%s\" />" % (self.session_id)
-		post_data = (xml_string).encode('utf-8')
-
-		# Prepare the request.
-		request = urllib.request.Request(self.url)
-		request.add_header("Content-type", "text/xml")
-
-		# Get a response.
-		response = urllib.request.urlopen(request, post_data, context=self.ctx).read()
-		xml_response = etree.fromstring(response)
-
-		# Check for errors.
-		if not xml_response.tag == 'Failure':
-			pass
-		else:
-			for exception in xml_response.iter('Exception'):
-				for message in exception.iter('Message'):
-					return str("Failure: " + message.text)
+		xml_response = self.api_request(xml_string)
 
 if __name__ == '__main__':
 	# Usage: ./nexpose.py hostname port username password
 	nexpose = Nexpose(sys.argv[1], sys.argv[2])
 	result = nexpose.login(sys.argv[3], sys.argv[4])
-	if result:
-		print(result)
 	if nexpose.session_id:
 		print(nexpose.session_id)
 		nexpose.logout()
